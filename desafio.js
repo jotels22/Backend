1be445ac413610595ba8e9596903bd39ba05cbc6
Juan Ignacio Rodriguez <sylcaty147@gmail.com>
2023-11-08 22:19:44 -0300

    1era entrega

--- desafio.js
+++ desafio.js
@@ -1,3 +1,6 @@
+const { log } = require("console");
+const { title } = require("process");
+
 class ProductManager {
     constructor() {
       this.products = [];
@@ -7,36 +10,53 @@ class ProductManager {
       return this.products;
     }
 
-    addProducts(title,description,price){
+    addProduct(title,description,price,thumbnail,code,stock,){
 
         let id_product = (this.getProducts()).length
 
+
         let product = {
-            title: "producto prueba",
-            description: "Este es un producto prueba",
-            price: 200,
-            thumbnail:"Sin imágen",
-            code:"abc123",
-            stock:25,
+            title:title,
+            description:description,
+            price: price,
+            thumbnail:thumbnail,
+            code:code,
+            stock:stock,
             id:id_product++
         }
+
+        // product = {
+        //     title:"title",
+        //     description:"description",
+        //     price: "price",
+        //     thumbnail:"thumbnail",
+        //     code:"code",
+        //     stock:"stock",
+        //     id:id_product++
+        // }
 
         this.products.push(product) 
         return this.products
     }
 
-    getProductById(idProducto){
-        let producto = this.products.find(evento => producto.id == idProducto)
-        if(producto){
-            return producto
-        } else {
+    traerProducto(idProducto){
+        let evento = this.eventos.find(event => event.id == idProducto)
+        if (evento) {
+            return evento
+        } else{
             return null
         }
-        
     }
 
-    
+}
 
-    }
 
+const productManagerInstance = new ProductManager();
+
+const producto2 = productManagerInstance.addProduct("producto prueba","Este es un producto prueba",200,"Sin imagen","abc123",25)
+
+console.log(producto2)
+
+
+;
 
